655e4ff14beeaca2e5e0eefc4e971dc755aff4e6
Jose Antonio Fernandes de Macedo <jose.macedo@dc.ufc.br>
2021-01-22 11:38:18 -0300

    Synch 2

--- 4_Lab_Sincronizacao/SynchClass.java
+++ 4_Lab_Sincronizacao/SynchClass.java
@@ -15,4 +15,27 @@ public class SynchClass {
   public static void nonSynchStaticMethod(int i) { 
      while (true) System.out.println(i);
   }
+
+  private Object lock1 = new Object();
+  private Object lock2 = new Object();
+
+  public void synchThisMethod(int i) {
+     synchronized (this) {
+       while (true) System.out.println(i);
+     }
+  }
+
+  public void synchLock1Method(int i) {
+     synchronized (lock1) { 
+       while (true) System.out.println(i);
+     }
+  }
+
+  public void synchLock2Method(int i) {
+     synchronized (lock2) { 
+       while (true) System.out.println(i);
+     }
+  }
+
+
 }
