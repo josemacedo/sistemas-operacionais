b35bddb6d03ba4a06958ef8fdfe1bfb46e142ee8
Jose Antonio Fernandes de Macedo <jose.macedo@dc.ufc.br>
2021-01-22 10:59:24 -0300

    Lab Sync

--- 4_Lab_Sincronizacao/ExemploSemaforo.java
+++ 4_Lab_Sincronizacao/ExemploSemaforo.java
@@ -1,12 +1,18 @@
-/**
- * 
- */
+import java.util.concurrent.Semaphore;
 
-/**
- * @author Vinicius Pires de Moura Freire
- *
- */
 public class ExemploSemaforo
 {
+  private int value = 0;
+
+    private final Semaphore mutex = new Semaphore(1);
+
+    public int getNextValue() throws InterruptedException {
+        try {
+            mutex.acquire();
+            return ++value;
+        } finally {
+            mutex.release();
+        }
+    }
 
 }
